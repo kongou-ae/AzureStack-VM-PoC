b62900f449f3cdb0813677c2b0db0438365936b3
kongou-ae <y.matsumoto.ae@gmail.com>
2020-11-15 00:14:44 +0900

    Update Create-AzSCustomImage.ps1

--- ARMv2/Create-AzSCustomImage.ps1
+++ ARMv2/Create-AzSCustomImage.ps1
@@ -4,11 +4,11 @@
 Install-WindowsFeature Hyper-V -IncludeManagementTools -Restart
 
 $defaultLocalPath = "C:\AzureStackOnAzureVM"
-$versionContainerName = "1910-58"
+$versionContainerName = "2008.13"
 $version = $versionContainerName.split("-")[0]
 
 New-Item -Path $defaultLocalPath -ItemType Directory
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/yagmurs/AzureStack-VM-PoC/development/scripts/ASDKHelperModule.psm1" -OutFile "$defaultLocalPath\ASDKHelperModule.psm1"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/kongou-ae/AzureStack-VM-PoC/development/scripts/ASDKHelperModule.psm1" -OutFile "$defaultLocalPath\ASDKHelperModule.psm1"
 Import-Module "$defaultLocalPath\ASDKHelperModule.psm1" -Force
 $asdkDownloadPath = "d:\"
 $asdkExtractFolder = "Azure Stack Development Kit"
